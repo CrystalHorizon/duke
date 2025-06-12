a1bd50e1b70a164579e4c648d760e3908db22f1b
Freestyle <148187561+CrystalHorizon@users.noreply.github.com>
2025-06-12 06:45:33 +0300

    gray filter
    Завантаження зображення та застосування сірого фільтра

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+var image;
+var grayscale;
+
 function upload() {
     var imgcanvas = document.getElementById('can');
+    var imgcanvas2 = document.getElementById("can2");
     var fileinput = document.getElementById('finput');
-    var image = new SimpleImage(fileinput);
+    image = new SimpleImage(fileinput);
+    grayscale = new SimpleImage(fileinput); 
     image.drawTo(imgcanvas);
+    image.drawTo(imgcanvas2);
+}
+
+function makeGray() {
+    for (var pixel of image.values()) {
+        var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
+        pixel.setRed(avg);
+        pixel.setGreen(avg);
+        pixel.setBlue(avg);
+    }
+    var imgcanvas2 = document.getElementById('can2');
+    image.drawTo(imgcanvas2);
 }
